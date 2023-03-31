2dc67b6a4abc420c4a9e52d3a4cbf817e70dc262
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2023-03-31 13:40:53 -0400

    pass 2nd stage again

--- app/main.py
+++ app/main.py
@@ -9,8 +9,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    clientSocket, address = server_socket.accept()  # wait for client
-    clientSocket.send("+PONG\r\n".encode())
+    clientSocket, _ = server_socket.accept()  # wait for client
+    clientSocket.recv(1024)
+    clientSocket.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
